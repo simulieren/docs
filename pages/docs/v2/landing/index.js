bfc22e3e4b708d6a88565932955841845807084d
Allen Hai <allenhai03@gmail.com>
2019-06-21 18:24:52 -0500

    add searchbar and cta in hero section

--- pages/docs/v2/landing/index.js
+++ pages/docs/v2/landing/index.js
@@ -1,16 +1,66 @@
+import { useAmp } from 'next/amp'
+import { useRouter } from 'next/amp'
+import algoliasearch from 'algoliasearch/lite'
+import { InstantSearch, Configure } from 'react-instantsearch-dom'
+import AutoComplete from '../../../../components/search'
 import Layout from '../../../../components/layout/layout'
+import Button from '../../../../components/buttons'
 
-export default function Landing() {
+function getAlgoliaClient() {
+  const algoliaClient = algoliasearch(
+    'NNTAHQI9C5',
+    'ac5d89f9877f9fb09dbdc9a010cca761'
+  )
+
+  return {
+    async search(requests) {
+      if (requests.every(({ params: { query } }) => Boolean(query) === false)) {
+        return {
+          results: requests.map(() => {
+            return {
+              processingTimeMS: 0,
+              nbHits: 0,
+              hits: [],
+              facets: {}
+            }
+          })
+        }
+      }
+
+      return algoliaClient.search(requests)
+    },
+    async searchForFacetValues(requests) {
+      return algoliaClient.searchForFacetValues(requests)
+    }
+  }
+}
+
+const searchClient = getAlgoliaClient()
+
+function Landing() {
   return (
     <Layout>
       <main>
         <section className="hero-section">
           <h1>Docs</h1>
+          <div className="desktop-search">
+            <InstantSearch indexName="prod_docs" searchClient={searchClient}>
+              <Configure hitsPerPage={8} />
+              <AutoComplete />
+            </InstantSearch>
+          </div>
+          <div className="get-started-cta">
+            <Button bgColor="#007aff" width="fit-content">
+              Get started with our quick introduction
+            </Button>
+          </div>
         </section>
       </main>
       <style jsx>{`
         .hero-section {
+          position: relative;
           display: flex;
+          flex-direction: column;
           justify-content: center;
           align-items: center;
           height: 335px;
@@ -19,7 +69,15 @@ export default function Landing() {
         .hero-section h1 {
           font-size: 36px;
         }
+        .get-started-cta {
+          position: absolute;
+          bottom: -20px;
+          left: 50%;
+          transform: translate(-50%);
+        }
       `}</style>
     </Layout>
   )
 }
+
+export default Landing
