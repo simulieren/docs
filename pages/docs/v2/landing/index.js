9510673dd5cf5962444649e00251675e655a6e95
Allen Hai <allenhai03@gmail.com>
2019-06-21 12:48:10 -0500

    create hero section with centered header

--- pages/docs/v2/landing/index.js
+++ pages/docs/v2/landing/index.js
@@ -1,5 +1,25 @@
 import Layout from '../../../../components/layout/layout'
 
 export default function Landing() {
-  return <Layout />
+  return (
+    <Layout>
+      <main>
+        <section className="hero-section">
+          <h1>Docs</h1>
+        </section>
+      </main>
+      <style jsx>{`
+        .hero-section {
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          height: 335px;
+          background-color: rgba(250, 250, 250);
+        }
+        .hero-section h1 {
+          font-size: 36px;
+        }
+      `}</style>
+    </Layout>
+  )
 }
